e85a3b7fec1e52630f6a4afe4f317494a2e7e476
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:12:34 +0100

    Add project files.

---
+++ Dati_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dati_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
