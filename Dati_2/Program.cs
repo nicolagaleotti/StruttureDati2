65faa27a2483ca4d089bdc636ddd2751088b720e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:32:20 +0100

    fine

--- Dati_2/Program.cs
+++ Dati_2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Dati_2
 {
@@ -6,7 +8,37 @@ namespace Dati_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string file = "interi.txt";
+            List<int> votiList = new List<int>();
+            if(File.Exists(file))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                            votiList.Add(int.Parse(line));
+                    }
+                } catch { }
+            }
+            string file2 = "interi2.txt";
+            List<int> votiList2 = new List<int>();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(file2))
+                {
+                    for (int i = 0; i < votiList.Count; i++)
+                    {
+                        if ( ! votiList2.Contains(votiList[i]))
+                        {
+                            writer.WriteLine(votiList[i]);
+                            votiList2.Add(votiList[i]);
+                        }
+                    }
+                        
+                }
+            } catch { }
         }
     }
 }
